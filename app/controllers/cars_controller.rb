5862b999164504f759c134612b6065e73c357d26
Gaetan Tripelon <tripelong@gmail.com>
2023-05-27 17:21:01 +0200

    Cars controller actions edited

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,10 +1,10 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:show, :edit, :update, :destroy]
   def index
     @cars = Car.all
   end
 
   def show
-    @car = Car.find(params[:id])
   end
 
   def new
@@ -21,7 +21,25 @@ class CarsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @car.update(car_params)
+    redirect_to car_path(@car)
+  end
+
+  def destroy
+    @car.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to cars_path, status: :see_other
+  end
+
   private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
 
   def car_params
     params.require(:car).permit(:name, :description, :location, :price)
