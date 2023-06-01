c14a9a655db2185ec81c1bce8089b55f3081dd1e
Alexis Florentin <alexisflorentin14@gmail.com>
2023-06-01 20:17:58 +0200

    alex part

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,19 +2,22 @@ class CarsController < ApplicationController
   before_action :set_car, only: %i[show edit update destroy]
   skip_before_action :authenticate_user!, only: %i[index show]
   def index
-    @cars = Car.all
+    @cars = policy_scope(Car)
   end
 
   def show
+    authorize @car
   end
 
   def new
     @car = Car.new
+    authorize @car
   end
 
   def create
     @car = Car.new(car_params)
     @car.user = current_user
+    authorize @car
     if @car.save
       redirect_to car_path(@car)
     else
@@ -23,15 +26,18 @@ class CarsController < ApplicationController
   end
 
   def edit
+    authorize @car
   end
 
   def update
     @car.update(car_params)
     redirect_to car_path(@car)
+    authorize @car
   end
 
   def destroy
     @car.destroy
+    authorize @car
     # No need for app/views/restaurants/destroy.html.erb
     redirect_to cars_path, status: :see_other
   end
