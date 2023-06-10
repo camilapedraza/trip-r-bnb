52ef4453975a18dc56fa32138abf3276d1ef0e32
Camila Pedraza <36695574+camilapedraza@users.noreply.github.com>
2023-06-10 16:12:01 +0200

    delete and modify bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,10 +4,10 @@ class Booking < ApplicationRecord
   validates :user, :car, :starting_date, :ending_date, presence: true
   validates :starting_date, comparison: {
     greater_than_or_equal_to: Date.today,
-    message: "Start date must be in the future"
+    message: "must be in the future"
   }
   validates :ending_date, comparison: {
     greater_than_or_equal_to: :starting_date,
-    message: "End date must be on or after start date"
+    message: "must be on or after starting date"
   }
 end
